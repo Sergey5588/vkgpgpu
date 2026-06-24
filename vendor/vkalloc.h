5e0ddbf72d67a9f7fd13dd4de1c66272fc459b27
Sergey5588 <sergey271109@gmail.com>
2026-06-24 18:09:42 +0300

    fix SIGSEGV: changed vkalloc to include volk instead of vulkan

--- vendor/vkalloc.h
+++ vendor/vkalloc.h
@@ -20,11 +20,14 @@ freely, subject to the following restrictions:
 3. This notice may not be removed or altered from any source distribution.
 */
 
+/* Modified version:
+   - Replaced inclusion of <vulkan.h> with inclusion of "volk.h"
+   Original file copyright: Leon Marz <main@lmarz.org>
+*/
 #ifndef VKALLOC_H
 #define VKALLOC_H
 
-#include <vulkan/vulkan.h>
-
+#include "volk.h"
 
 enum mem_type {
 	MEM_TYPE_STAGING, /* Memory that is accessible from the CPU and GPU */
