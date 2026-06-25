fe023c43a2756b9bdb474be7abb49075f536a2bd
Sergey5588 <sergey271109@gmail.com>
2026-06-25 20:02:18 +0300

    update example

--- example/main.c
+++ example/main.c
@@ -1,19 +1,17 @@
-#define _GNU_SOURCE // for usleep
 #include "../include/vkgpgpu.h"
-#include <unistd.h> // for usleep, just for tests
 int main() {
 	GpuContext *ctx = gpu_ctx_init();
-	GpuBuffer *buf1 = gpu_buf_create(ctx, 1337*sizeof(int32_t),0);
-	GpuBuffer *buf2 = gpu_buf_create(ctx, 69420*sizeof(int32_t), 0);
-	int32_t* data1;
-	int32_t* data2;
+	GpuBuffer *buf1 = gpu_buf_create(ctx, 1337*sizeof(uint32_t),0);
+	GpuBuffer *buf2 = gpu_buf_create(ctx, 1337*sizeof(uint32_t), 0);
+	uint32_t *data1;
+	uint32_t *data2;
 
 	gpu_buf_map(buf1,(void**)&data1);
 	gpu_buf_map(buf2,(void**)&data2);
 	data1[0] = 1123123123;
-	usleep(60000000); // simulate some hard work
 	gpu_buf_unmap(buf1);
 	gpu_buf_unmap(buf2);
+	GpuKernel *k = gpu_kernel_create(ctx, "./shaders/copy.comp.spv", 2, buf1, buf2);
 	gpu_buf_destroy(buf1);
 	gpu_buf_destroy(buf2);
 	gpu_ctx_destroy(ctx);
