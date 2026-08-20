7e8f8e205d04a7a8c7a36538df42d85e6e4562b1
Sergey5588 <sergey271109@gmail.com>
2026-08-20 14:03:27 +0300

    add comments to example

--- example/main.c
+++ example/main.c
@@ -9,12 +9,13 @@ int main() {
 
 	gpu_buf_map(buf1,(void**)&data1);
 	gpu_buf_map(buf2, (void**)&data2);
-	for(size_t i = 0; i < 1337; i++) data1[i] = i+1;
+	for(size_t i = 0; i < 1337; i++) data1[i] = i+1; // fill data
 
 	GpuProgram *p = gpu_program_load(ctx, "./shaders/test.comp.spv");
 	float alpha = 2.3f;
 	float beta  = 3.7f;
-	GpuCommand *cmd = gpu_command_begin(ctx);
+
+	GpuCommand *cmd = gpu_command_begin(ctx); // start recording
 	gpu_command_bind_buffer(cmd,0, buf1);
 	gpu_command_bind_buffer(cmd,1, buf2);
 	
