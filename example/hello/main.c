da28deaa08daa9537a528308e3ee8dac6546c5a0
Sergey5588 <sergey271109@gmail.com>
2026-09-12 17:07:04 +0300

    update example

--- example/hello/main.c
+++ example/hello/main.c
@@ -2,38 +2,29 @@
 #include <stdio.h>
 int main() {
 	GpuContext *ctx = gpu_ctx_init();
-	GpuBuffer *buf1 = gpu_buf_create(ctx, 1337*sizeof(float), MEM_STAGING);
-	GpuBuffer *buf2 = gpu_buf_create(ctx, 1337*sizeof(float), MEM_STAGING);
-	float *data1;
-	float *data2;
-
-	gpu_buf_map(buf1,(void**)&data1);
-	gpu_buf_map(buf2, (void**)&data2);
-	for(size_t i = 0; i < 1337; i++) data1[i] = i+1; // fill data
-
-	GpuProgram *p = gpu_program_load(ctx, "./shaders/test.comp.spv");
-	float alpha = 2.3f;
-	float beta  = 3.7f;
+	GpuBuffer *buffer = gpu_buf_create(ctx, 1337*sizeof(float), MEM_STAGING);
+	float *data;
+	gpu_buf_map(buffer,(void**)&data);
+	for(size_t i = 0; i < 1337; i++) data[i] = i+1; // fill data
+	printf("Sent to GPU: %f, %f, %f ...\n", data[0], data[1], data[2]);
+	
+	GpuProgram *p = gpu_program_load(ctx, "./shaders/test.comp.spv"); // load shader from disk
+	float alpha = 67.0f;
 
 	GpuCommand *cmd = gpu_command_begin(ctx); // start recording
-	gpu_command_bind_buffer(cmd,0, buf1);
-	gpu_command_bind_buffer(cmd,1, buf2);
+	gpu_command_bind_buffer(cmd,0, buffer);
 	
-	gpu_set(cmd, p, alpha);
-	gpu_set(cmd, p, beta);
+	gpu_set(cmd, p, alpha); // push constant
 
 	gpu_command_dispatch(cmd, p, 7,1,1);
 	gpu_command_submit(cmd); // wait for result
 
-	printf("First values of buf1: %f, %f, %f\n", data1[0], data1[1], data1[2]);
-	printf("First values of buf2: %f, %f, %f\n", data2[0], data2[1], data2[2]);
-
+	printf("Got: %f, %f, %f\n", data[0], data[1], data[2]); // Expected 67.000000, 134.000000, 201.000000
 
 	//cleanup
 	gpu_command_destroy(cmd);
 	gpu_program_destroy(p);
-	gpu_buf_destroy(buf1);
-	gpu_buf_destroy(buf2);
+	gpu_buf_destroy(buffer);
 	gpu_ctx_destroy(ctx);
 	return 0;
 }
