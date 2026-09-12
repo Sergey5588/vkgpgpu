4e6162db115fb239f9b20f76109968faedcb2eef
Sergey5588 <sergey271109@gmail.com>
2026-09-12 17:24:15 +0300

    todo.h improvements

--- src/todo.h
+++ src/todo.h
@@ -1,8 +1,12 @@
-#ifndef __TODO_H
-#define __TODO_H
+#ifndef _TODO_H
+#define _TODO_H
 #include <stdio.h>
+#ifdef VKGPGPU_DEBUG
 #define TODO(msg) do { \
 	fprintf(stderr, "TODO at %s:%d: %s\n", __FILE__, __LINE__, msg); \
-} while(0) \
+} while(0)
+#else
+#define TODO(msg)
+#endif
 
 #endif
